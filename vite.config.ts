7231f69aeb7d86157b0ae24f0f20f35757e5bd3c
Youzih <63506018+yu320@users.noreply.github.com>
2026-02-26 02:49:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/give-me-money-discord/', // 請替換成您的 Repo 名稱，例如 '/givememoney/'
+  base: '/', // 請替換成您的 Repo 名稱，例如 '/givememoney/'
 })
+
